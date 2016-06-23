cc67252618140d4b5f7240ec58fd39a5afde737a
Chris Choi <cmhchoi@gmail.com>
2016-06-22 20:55:08 -0700

    initiate server

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "env": {
+    "es6": true
+  },
+  "extends": "airbnb",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+};
