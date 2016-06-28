050d9d4cfd173cfa58225239a1bd87e80b9a0494
Chris Choi <cmhchoi@gmail.com>
2016-06-27 17:01:18 -0700

    db config process.env

--- server/auth/config.js
+++ server/auth/config.js
@@ -4,7 +4,7 @@ module.exports = {
   facebookAuth: {
     // clientID: 699168510234037,
     clientSecret: 'fc8e52b297b8d090dba37bf2cfa01afa',
-    clientID: process.env.fb_clientID,
+    clientID: 699168510234037,
     // clientSecret: process.env.fb_clientSecret || secret.fb_clientSecret,
     callbackURL: 'http://localhost:3000/auth/facebook/callback',
   },
