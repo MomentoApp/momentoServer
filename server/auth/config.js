1b64440eba0add2bc74b5646fefe43df1ae4d88c
Chris Choi <cmhchoi@gmail.com>
2016-06-27 18:49:22 -0700

    fb process.env

--- server/auth/config.js
+++ server/auth/config.js
@@ -3,10 +3,12 @@
 module.exports = {
   facebookAuth: {
     // clientID: 699168510234037,
-    clientID: 700922706725284,
-    clientSecret: '78f7f1063669d29dc61ac114ece1914b',
-    // clientSecret: process.env.fb_clientSecret || secret.fb_clientSecret,
-    callbackURL: 'http://momento-4.gnzjvesgnp.us-west-2.elasticbeanstalk.com/auth/facebook/callback',
+    // clientID: 700922706725284,
+    clientID: process.env.fb_clientID,
+    // clientSecret: '78f7f1063669d29dc61ac114ece1914b',
+    clientSecret: process.env.fb_clientSecret,
+    // callbackURL: 'http://momento-4.gnzjvesgnp.us-west-2.elasticbeanstalk.com/auth/facebook/callback',
+    callbackURL: process.env.fb_callbackURL,
   },
   googleAuth: {
     clientID: '',
