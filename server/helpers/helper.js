14c269541de6d603833fd3d3fd101b0bd9c84a2b
Chris Choi <cmhchoi@gmail.com>
2016-07-01 19:48:54 -0700

    got rid of token

--- server/helpers/helper.js
+++ server/helpers/helper.js
@@ -6,26 +6,27 @@ module.exports = {
     console.log('HEADER DETAILS', 
       [req.get('token'), req.get('id'), req.get('name'), req.get('pic')]
     );
-    User.checkToken(req.get('token'), (err, data) => {
-      if (err) throw err;
-      if (data) {
-        next();
-      } else {
-        https.get(`https://graph.facebook.com/${req.get('id')}/permissions?access_token=${req.get('token')}`, innerRes => {
-          let body = '';
-          innerRes.on('data', chunk => body += chunk);
-          innerRes.on('end', () => {
-            if(JSON.parse(body).data) {
-              User.update(req.get('id'), req.get('token'), (err, data) => {
-                if (err) throw err;
-                if (data) next();
-              })
-            } else {
-              outerRes.end('Permission denied')
-            }
-          })
-        })
-      }
-    })
+    // User.checkToken(req.get('token'), (err, data) => {
+    //   if (err) throw err;
+    //   if (data) {
+    //     next();
+    //   } else {
+    //     https.get(`https://graph.facebook.com/${req.get('id')}/permissions?access_token=${req.get('token')}`, innerRes => {
+    //       var body = '';
+    //       innerRes.on('data', chunk => body += chunk);
+    //       innerRes.on('end', () => {
+    //         if(JSON.parse(body).data) {
+    //           User.update(req.get('id'), req.get('token'), (err, data) => {
+    //             if (err) throw err;
+    //             if (data) next();
+    //           })
+    //         } else {
+    //           outerRes.end('Permission denied')
+    //         }
+    //       })
+    //     })
+    //   }
+    // })
+    next();
   },
 };
